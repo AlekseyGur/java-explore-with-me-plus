6c86c14ae86dc1f54e377ebacfe601558ea57b95
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-06-11 13:25:28 +0300

    Fix api error

---
+++ main-service/src/main/java/ru/practicum/main/system/exception/ApiError.java
@@ -0,0 +1,21 @@
+package ru.practicum.main.system.exception;
+
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiError {
+    private HttpStatus status;
+    private String reason; // Общее описание причины ошибки
+    private String message; // Сообщение об ошибке
+    private LocalDateTime timeStamp = LocalDateTime.now();
+
+    public ApiError(HttpStatus status, String reason, String message) {
+        this.status = status;
+        this.reason = reason;
+        this.message = message;
+    }
+}
